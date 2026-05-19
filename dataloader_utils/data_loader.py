c4c7c8c49da8f48854f726886c82f40bc72e011c
Andcha37 <andcha37@gmail.com>
2026-05-20 03:08:16 +0900

    update

--- dataloader_utils/data_loader.py
+++ dataloader_utils/data_loader.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader, random_split
 from dataset_utils.dataset import FullDataset
 import copy
 
-def get_dataloaders(clean_dir, noise_dir, batch_size=16, num_workers=2, split_ratio=(0.8, 0.1, 0.1)):
+def get_dataloaders(clean_dir, noise_dir, batch_size=16, num_workers=4, split_ratio=(0.8, 0.1, 0.1), worker_init_fn=None, generator=None):
     fulldataset = FullDataset(
         clean_seg_dir = clean_dir,
         noise_dir = noise_dir,
@@ -31,7 +31,9 @@ def get_dataloaders(clean_dir, noise_dir, batch_size=16, num_workers=2, split_ra
         shuffle=True,
         num_workers=num_workers,
         pin_memory=True,
-        drop_last=True
+        drop_last=True,
+        worker_init_fn=worker_init_fn,
+        generator=generator
     )
 
     val_dataset.dataset = copy.copy(fulldataset)
@@ -45,7 +47,9 @@ def get_dataloaders(clean_dir, noise_dir, batch_size=16, num_workers=2, split_ra
         batch_size=batch_size,
         shuffle=False,
         num_workers=num_workers,
-        pin_memory=True
+        pin_memory=True,
+        worker_init_fn=worker_init_fn,
+        generator=generator
     )
 
     test_loader = DataLoader(
@@ -53,10 +57,9 @@ def get_dataloaders(clean_dir, noise_dir, batch_size=16, num_workers=2, split_ra
         batch_size=batch_size,
         shuffle=False,
         num_workers=num_workers,
-        pin_memory=True
+        pin_memory=True,
+        worker_init_fn=worker_init_fn,
+        generator=generator
     )
 
     return train_loader, val_loader, test_loader
-
-
-
