f0e1daaba39b86c29d9186e1b96b97b31dd1b46f
Andcha37 <andcha37@gmail.com>
2026-05-19 16:51:16 +0900

    add new folder

---
+++ postprocess_utils/audio_postprocess.py
@@ -0,0 +1,19 @@
+import torch
+
+
+def inverse_stft(log_magnitude, phase, n_fft=512, hop_length=256):
+    magnitude = (10 ** log_magnitude) - 1
+
+    pad_mag = torch.cat([magnitude, torch.zeros(magnitude.shape[0], 1, magnitude.shape[-1]).to(magnitude.device)], dim=1)
+    pad_phase = torch.cat([phase, torch.zeros(phase.shape[0], 1, phase.shape[-1]).to(phase.device)], dim=1)
+
+    stft_matrix = pad_mag * torch.exp(1j * pad_phase)
+    window = torch.hann_window(n_fft).to(stft_matrix.device)
+
+    restored_audio = torch.istft(
+        stft_matrix, 
+        n_fft=n_fft, 
+        hop_length=hop_length, 
+        window=window
+    )
+    return restored_audio
